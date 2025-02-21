12f378f05cb925d1b7baa3103ea2dcbe4b890cb6
Z Yin <Z.Yin.MD@gmail.com>
2025-02-21 18:28:55 -0500

    new scaffolding with sveltekit + tailwind + shadcn

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import ShadcnMenu from "./ShadcnMenu.svelte";
+</script>
+
+<ShadcnMenu />
---
+++ src/routes/ShadcnMenu.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+</script>
+
+<div class="mt-8 mr-8">
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger>Open</DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Group>
+        <DropdownMenu.Item onclick={() => console.log("new game")}
+          >New game</DropdownMenu.Item
+        >
+        <DropdownMenu.Item onclick={() => console.log("settings")}>
+          Settings
+        </DropdownMenu.Item>
+        <DropdownMenu.Item onclick={() => console.log("practice mode")}>
+          Practice mode
+        </DropdownMenu.Item>
+      </DropdownMenu.Group>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
+</div>
